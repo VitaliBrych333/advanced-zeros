f64d1ae33c60ccdf05819dd6dceca84ae8f73dba
Vitali Br <epamc@mail.ru>
2018-02-24 01:19:51 +0300

    Zeros with base

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-	var n = number;
-	var k = base;
-    function f( n,  i) { return n ? n/i + Math.floor(f(n/i, i)) : 0; }
+	var num = number;
+	var bas = base;
+	var count=num;
+
+
+    function f( num,  i) 
+      { return num ? num/i + Math.floor(f(num/i, i)) : 0; }
  
-    var r=n;
-    for (var i=2; i*i<=k; i++) {
-        var p=0;
-        while (k%i==0) {p++; k/=i;}
-        if (p) r = Math.floor(Math.min(r, Math.floor(f(n,i))/p));
+    
+    for (var i=2; i*i<=bas; i++) {
+        var t=0;
+        while (bas%i==0) {t++; bas/=i;}
+        if (t) count = Math.floor(Math.min(count, Math.floor(f(num,i))/t));
     }
-    if (k>1) r = Math.min(r, Math.floor(f(n,k)));
-    return r;
+
+    if (bas>1) count = Math.min(count, Math.floor(f(num,bas)));
+    return count;
 
 }
 
