e512e16da3cc76b8591512be3b13e032ab366488
Vitali Br <epamc@mail.ru>
2018-02-24 01:08:46 +0300

    Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var n = number;
+	var k = base;
+    function f( n,  i) { return n ? n/i + Math.floor(f(n/i, i)) : 0; }
+ 
+    var r=n;
+    for (var i=2; i*i<=k; i++) {
+        var p=0;
+        while (k%i==0) {p++; k/=i;}
+        if (p) r = Math.floor(Math.min(r, Math.floor(f(n,i))/p));
+    }
+    if (k>1) r = Math.min(r, Math.floor(f(n,k)));
+    return r;
+
 }
+
+
+ 
